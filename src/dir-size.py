db0f6a9058d804c689a2891bd24d5258bdf44be6
Evgeniy Turishev <evgeniy.turishev@area9.dk>
2025-01-25 12:12:30 +0300

    FileSizeList class

--- src/dir-size.py
+++ src/dir-size.py
@@ -5,9 +5,62 @@ import gi
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw, Gio, Gdk, Graphene, GLib
+from gi.repository import Gtk, Adw, Gio, Gdk, Graphene, GLib, GObject
 
-# https://python-gtk-3-tutorial.readthedocs.io/en/latest/application.html
+
+class DataObject(GObject.GObject):
+    #__gtype_name__ = 'DataObject'
+    text = GObject.Property(type=str, default=None)
+
+    def __init__(self, text, number):
+        super().__init__()
+        self.text = text
+        self.number = number
+
+
+class FileSizeList():
+    def __init__(self):
+        self.store = Gio.ListStore.new(DataObject)
+        sel_model = Gtk.SingleSelection.new(self.store)
+        self.list_view = Gtk.ColumnView.new(sel_model)
+
+        factory_c1 = Gtk.SignalListItemFactory()
+        factory_c1.connect("setup", self._setup_c1)
+        factory_c1.connect("bind", self._bind_c1)
+
+        factory_c2 = Gtk.SignalListItemFactory()
+        factory_c2.connect("setup", self._setup_c2)
+        factory_c2.connect("bind", self._bind_c2)
+
+        self.list_view = Gtk.ColumnView.new(sel_model)
+        self.list_view.append_column(Gtk.ColumnViewColumn.new("size", factory_c1))
+        self.list_view.append_column(Gtk.ColumnViewColumn.new("name", factory_c2))
+        self.list_view.connect ("activate", self._activate_cb);
+
+    def _setup_c1(self, factory, item):
+        label = Gtk.Label()
+        item.set_child(label)
+
+    def _bind_c1(self, factory, item):
+        label = item.get_child()
+        obj = item.get_item()
+        label.set_text(obj.number)
+
+    def _setup_c2(self, factory, item):
+        label = Gtk.EditableLabel()
+        item.set_child(label)
+
+    def _bind_c2(self, factory, item):
+        label = item.get_child()
+        obj = item.get_item()
+        label.set_text(obj.text)
+        label.bind_property("text", obj, "text")
+
+    def _activate_cb(self): pass
+
+    def append(self, name, size):
+        self.store.append(DataObject(name, str(size)))
+
 
 class MainWindow(Gtk.ApplicationWindow):
     app_title = "dir-size"
@@ -18,34 +71,25 @@ class MainWindow(Gtk.ApplicationWindow):
         self.set_title(self.app_title)
 
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.main_box)
+
         self.center_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.bottom_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-
-
         self.center_box.set_vexpand(True)
+        self.main_box.append(self.center_box)
 
+        self.bottom_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.bottom_box.set_spacing(10)
         self.bottom_box.set_margin_top(10)
         self.bottom_box.set_margin_bottom(10)
         self.bottom_box.set_margin_start(10)
         self.bottom_box.set_margin_end(10)
         self.bottom_box.set_homogeneous(True)
-
-        self.set_child(self.main_box)
-        self.main_box.append(self.center_box)
         self.main_box.append(self.bottom_box)
 
-        self.result_list_model = Gtk.StringList.new() # model
-        self.result_list_model.append('xxx')
-        self.result_list_model.append('yyy')
-        sel_model = Gtk.SingleSelection.new(self.result_list_model)
-        factory = Gtk.SignalListItemFactory.new();
-        # factory.connect("setup", setup_listitem_cb);
-        # factory.connect("bind", bind_listitem_cb);
-
-
-        self.result_list = Gtk.ListView.new(sel_model, factory)
-        self.center_box.append(self.result_list)
+        self.result_list = FileSizeList()
+        self.center_box.append(self.result_list.list_view)
+        self.result_list.append("xxx", 123)
+        self.result_list.append("yyy", 456)
 
         self.open_bt = Gtk.Button(label="Select dir")
         self.open_bt.connect("clicked", self.show_open_dialog)
