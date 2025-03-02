0d6a38632f3d50d7fdd7dc78f2781b28ebe723b2
Evgeniy Turishev <evgeniy.turishev@area9.dk>
2025-03-02 14:03:10 +0300

    dirsize: fix size format

--- src/dirsize/utils.py
+++ src/dirsize/utils.py
@@ -2,5 +2,9 @@ import math
 
 def format_size(size : int):
     s = str(size)
-    parts = [s[-3:] if i == 0 else  s[-(i*3 + 3) : -i*3] for i in range(math.ceil(len(s) / 3))]
-    return '_'.join(parts)
+    if len(s) > 3:
+        parts = [s[-3:] if i == 0 else  s[-(i*3 + 3) : -i*3] for i in range(math.ceil(len(s) / 3))]
+        parts.reverse()
+        return '_'.join(parts)
+    else:
+        return s
